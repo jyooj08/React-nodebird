8dea82cfd8a115bb4935c390ea21cd9d3194516a
Yu Jin Jang <jyooj08@naver.com>
2022-02-14 12:01:19 +0900

    Add antd and Head

--- front/.next/static/chunks/webpack.js
+++ front/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bc5bf86d4c9e1d17"; }
+/******/ 		__webpack_require__.h = function() { return "be9e1cdfa09ce8f2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-
+import { Menu } from 'antd'
+;
 const AppLayout = ({ children }) => {
     return (
         <div>
-            <div>
-                <Link href='/'><a>NodeBird</a></Link>
-                <Link href='/profile'><a>Profile</a></Link>
-                <Link href='/signup'><a>Signup</a></Link>
-            </div>
+            <Menu>
+                <Menu.Item>
+                    <Link href='/'><a>NodeBird</a></Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href='/profile'><a>Profile</a></Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href='/signup'><a>Signup</a></Link>
+                </Menu.Item>
+            </Menu>
             {children}
         </div>
     );
 };
 
-AppLayout.PropTypes = {
+AppLayout.propTypes = {
     children: PropTypes.node.isRequired
 }
 
