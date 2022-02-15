2171479ca4f8da5606058b98ac730b6252e97a0c
Yu Jin Jang <jyooj08@naver.com>
2022-02-15 15:34:55 +0900

    Add grid menu

--- front/.next/static/chunks/webpack.js
+++ front/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "be9e1cdfa09ce8f2"; }
+/******/ 		__webpack_require__.h = function() { return "56709e626d1c55f0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import { Menu } from 'antd'
+import { Menu, Input, Row, Col } from 'antd'
 ;
 const AppLayout = ({ children }) => {
     return (
         <div>
-            <Menu>
+            <Menu mode='horizontal'>
                 <Menu.Item>
                     <Link href='/'><a>NodeBird</a></Link>
                 </Menu.Item>
@@ -14,10 +14,23 @@ const AppLayout = ({ children }) => {
                     <Link href='/profile'><a>Profile</a></Link>
                 </Menu.Item>
                 <Menu.Item>
+                    <Input.Search enterButton style={{verticalAlign:'middle'}}/>
+                </Menu.Item>
+                <Menu.Item>
                     <Link href='/signup'><a>Signup</a></Link>
                 </Menu.Item>
             </Menu>
-            {children}
+            <Row gutter={8}>
+                <Col xs={24} md={6}>
+                    왼쪽 메뉴
+                </Col>
+                <Col xs={24} md={12}>
+                    {children}
+                </Col>
+                <Col xs={24} md={6}>
+                    <a href='https://jyooj08.github.io/Portfolio/' target='_blank' rel='norefferrer noopener'>Made by Yujin😆</a>
+                </Col>
+            </Row>
         </div>
     );
 };
