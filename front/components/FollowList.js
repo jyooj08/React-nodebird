ed4b2f610bcbfa5960fec3827bb82c50fff9dcb2
Yu Jin Jang <jyooj08@naver.com>
2022-02-21 19:35:03 +0900

    Edit profile.js

---
+++ front/components/FollowList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Form, Input} from 'antd';
+
+const FollowList = ({header, data}) => {
+    return (
+        <List></List>
+    );
+}
+
+export default FollowList;
